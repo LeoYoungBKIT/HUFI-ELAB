524f429b6e7ceeaef436faaa28f14bc09a1f246e
toanhhg123 <toanhhg123@gmail.com>
2023-10-05 08:38:36 +0700

    reabse step 4 05/10/2023

--- src/services/employeeServices.ts
+++ src/services/employeeServices.ts
@@ -6,58 +6,58 @@ import { isArray } from "lodash";
 
 const { isProd } = config;
 const API_ENDPOINT = isProd
-  ? config.production.api_endpoint
-  : config.development.api_endpoint;
+    ? config.production.api_endpoint
+    : config.development.api_endpoint;
 
 export const getEmployees = async () => {
-  const url = `${API_ENDPOINT}/api/employees`;
-  const employees: IEmployeeType[] = await API.get<IEmployeeType[]>(url);
+    const url = `${API_ENDPOINT}/api/employees`;
+    const employees: IEmployeeType[] = await API.get<IEmployeeType[]>(url);
 
-  if (isArray(employees))
-    return employees?.map((item) => {
-      return Object.assign(
-        {},
-        {
-          ...item,
-          Birthdate: item.Birthdate,
-        }
-      );
-    });
+    if (isArray(employees))
+        return employees?.map((item) => {
+            return Object.assign(
+                {},
+                {
+                    ...item,
+                    Birthdate: item.Birthdate,
+                }
+            );
+        });
 
-  return [];
+    return [];
 };
 
 export const getEmployeeById = async (id: String) => {
-  const url = `${API_ENDPOINT}/api/employees/${id}`;
-  const employee: IEmployeeType = await API.get<IEmployeeType>(url);
-  return employee;
+    const url = `${API_ENDPOINT}/api/employees/${id}`;
+    const employee: IEmployeeType = await API.get<IEmployeeType>(url);
+    return employee;
 };
 
 export const updateEmployee = async (updatedData: IEmployeeType) => {
-  const url = `${API_ENDPOINT}/api/employees`;
-  const employee: IEmployeeType = await API.put<IEmployeeType, IEmployeeType>(
-    url,
-    updatedData
-  );
-  return employee;
+    const url = `${API_ENDPOINT}/api/employees`;
+    const employee: IEmployeeType = await API.put<IEmployeeType, IEmployeeType>(
+        url,
+        updatedData
+    );
+    return employee;
 };
 
 export const deleteEmployee = async (id: String) => {
-  const url = `${API_ENDPOINT}/api/employees/${id}`;
-  await API.deleteResource(url);
+    const url = `${API_ENDPOINT}/api/employees/${id}`;
+    await API.deleteResource(url);
 };
 
 export const postEmployee = async (employeesData: IEmployeeType) => {
-  const url = `${API_ENDPOINT}/api/employees`;
-  const employees: IEmployeeType = await API.post<IEmployeeType, IEmployeeType>(
-    url,
-    employeesData
-  );
-  return employees;
+    const url = `${API_ENDPOINT}/api/employees`;
+    const employees: IEmployeeType = await API.post<
+        IEmployeeType,
+        IEmployeeType
+    >(url, employeesData);
+    return employees;
 };
 
 export const getEmployeeOwner = async () => {
-  const url = `${API_ENDPOINT}/api/employees/owner`;
-  const owner: IUserOwner = await API.get<IUserOwner>(url);
-  return owner;
+    const url = `${API_ENDPOINT}/api/employees/owner`;
+    const owner: IUserOwner = await API.get<IUserOwner>(url);
+    return owner;
 };
