2dcf3e1e32421495304de477f36e6d466aacfb0c
toanhhg123 <toanhhg123@gmail.com>
2023-10-05 08:35:43 +0700

    reabse step 1 05/10/2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import {
-  Avatar,
-  IconButton,
-  Menu,
-  MenuItem,
-  Tooltip,
-  Typography,
+    Avatar,
+    IconButton,
+    Menu,
+    MenuItem,
+    Tooltip,
+    Typography,
 } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 import { Box } from "@mui/system";
@@ -19,9 +19,9 @@ import Footer from "./components/Footer";
 import { getFromLocalStorage } from "./configs/apiHelper";
 import { useAppDispatch, useAppSelector } from "./hooks";
 import {
-  logout,
-  setIsLogined,
-  setOwner,
+    logout,
+    setIsLogined,
+    setOwner,
 } from "./layouts/UserManager/userManagerSlice";
 import Account from "./pages/Account";
 import { setIsOpenDrawer } from "./pages/appSlice";
@@ -33,162 +33,176 @@ import { getLaboratories } from "./services/laboratoryServices";
 import { RootState } from "./store";
 
 const settings = [
-  {
-    text: "Tài khoản",
-    link: "/account",
-  },
-  {
-    text: "Đăng xuất",
-    link: "/logout",
-  },
+    {
+        text: "Tài khoản",
+        link: "/account",
+    },
+    {
+        text: "Đăng xuất",
+        link: "/logout",
+    },
 ];
 
 function App() {
-  const isOpenDrawer: boolean = useAppSelector(
-    (state: RootState) => state.app.isOpenDrawer
-  );
-  const isLogined = useAppSelector(
-    (state: RootState) => state.userManager.isLogined
-  );
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleDrawerOpen = () => {
-    dispatch(setIsOpenDrawer(true));
-  };
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  useEffect(() => {
-    getLaboratories();
-  }, []);
-
-  useEffect(() => {
-    const getUserLocal = async () => {
-      const user = await getFromLocalStorage("user");
-      if (user) {
-        dispatch(setOwner(user));
-        dispatch(setIsLogined(true));
-        navigate("/");
-      } else {
-        dispatch(logout());
-        dispatch(setIsLogined(false));
-        navigate("/login");
-      }
-    };
-    getUserLocal();
-  }, [isLogined]);
-
-  const _renderRouteElement = (id: String) => {
-    return (
-      <>
-        {id === "login" && <Login />}
-        {id === "notFound" && <NotFound />}
-        {(id === "account" || id === "dashboard") && (
-          <>
-            <AppBar position="fixed" open={isOpenDrawer}>
-              <Toolbar>
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={handleDrawerOpen}
-                  edge="start"
-                  sx={{
-                    marginRight: 5,
-                    ...(isOpenDrawer && { display: "none" }),
-                  }}
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" noWrap component="div">
-                  HUFI E-LAB
-                </Typography>
-
-                <Box sx={{ flexGrow: 0, position: "absolute", right: "3%" }}>
-                  <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      <Avatar
-                        alt="Remy Sharp"
-                        src="/static/images/avatar/2.jpg"
-                      />
-                    </IconButton>
-                  </Tooltip>
-                  <Menu
-                    sx={{ mt: "45px" }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
-                  >
-                    {settings.map((setting, index) => (
-                      <MenuItem
-                        key={index}
-                        onClick={handleCloseUserMenu}
-                        sx={{
-                          padding: 0,
-                        }}
-                      >
-                        <Link
-                          to={setting.link}
-                          style={{
-                            padding: "6px 16px",
-                            textDecoration: "none",
-                            color: "unset",
-                            width: "100%",
-                          }}
-                        >
-                          <Typography textAlign="center">
-                            {setting.text}
-                          </Typography>
-                        </Link>
-                      </MenuItem>
-                    ))}
-                  </Menu>
-                </Box>
-              </Toolbar>
-            </AppBar>
-            <div className="container">
-              <DrawerLeft />
-              {id === "dashboard" && <Dashboard />}
-              {id === "account" && <Account />}
-            </div>
-          </>
-        )}
-        <Footer />
-      </>
+    const isOpenDrawer: boolean = useAppSelector(
+        (state: RootState) => state.app.isOpenDrawer
     );
-  };
+    const isLogined = useAppSelector(
+        (state: RootState) => state.userManager.isLogined
+    );
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
 
-  return (
-    <div className="App">
-      <Routes>
-        <Route element={_renderRouteElement("login")} path="/login" />
-        <Route element={<Logout />} path="/logout" />
-        <Route element={_renderRouteElement("dashboard")} path="/" />
-        <Route element={_renderRouteElement("account")} path="/account" />
-        <Route element={_renderRouteElement("notFound")} path="*" />
-      </Routes>
-    </div>
-  );
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+        null
+    );
+
+    const handleDrawerOpen = () => {
+        dispatch(setIsOpenDrawer(true));
+    };
+
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    useEffect(() => {
+        getLaboratories();
+    }, []);
+
+    useEffect(() => {
+        const getUserLocal = async () => {
+            const user = await getFromLocalStorage("user");
+            if (user) {
+                dispatch(setOwner(user));
+                dispatch(setIsLogined(true));
+                navigate("/");
+            } else {
+                dispatch(logout());
+                dispatch(setIsLogined(false));
+                navigate("/login");
+            }
+        };
+        getUserLocal();
+    }, [isLogined]);
+
+    const _renderRouteElement = (id: String) => {
+        return (
+            <>
+                {id === "login" && <Login />}
+                {id === "notFound" && <NotFound />}
+                {(id === "account" || id === "dashboard") && (
+                    <>
+                        <AppBar position="fixed" open={isOpenDrawer}>
+                            <Toolbar>
+                                <IconButton
+                                    color="inherit"
+                                    aria-label="open drawer"
+                                    onClick={handleDrawerOpen}
+                                    edge="start"
+                                    sx={{
+                                        marginRight: 5,
+                                        ...(isOpenDrawer && {
+                                            display: "none",
+                                        }),
+                                    }}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Typography variant="h6" noWrap component="div">
+                                    HUFI E-LAB
+                                </Typography>
+
+                                <Box
+                                    sx={{
+                                        flexGrow: 0,
+                                        position: "absolute",
+                                        right: "3%",
+                                    }}
+                                >
+                                    <Tooltip title="Open settings">
+                                        <IconButton
+                                            onClick={handleOpenUserMenu}
+                                            sx={{ p: 0 }}
+                                        >
+                                            <Avatar
+                                                alt="Remy Sharp"
+                                                src="/static/images/avatar/2.jpg"
+                                            />
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Menu
+                                        sx={{ mt: "45px" }}
+                                        id="menu-appbar"
+                                        anchorEl={anchorElUser}
+                                        anchorOrigin={{
+                                            vertical: "top",
+                                            horizontal: "right",
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: "top",
+                                            horizontal: "right",
+                                        }}
+                                        open={Boolean(anchorElUser)}
+                                        onClose={handleCloseUserMenu}
+                                    >
+                                        {settings.map((setting, index) => (
+                                            <MenuItem
+                                                key={index}
+                                                onClick={handleCloseUserMenu}
+                                                sx={{
+                                                    padding: 0,
+                                                }}
+                                            >
+                                                <Link
+                                                    to={setting.link}
+                                                    style={{
+                                                        padding: "6px 16px",
+                                                        textDecoration: "none",
+                                                        color: "unset",
+                                                        width: "100%",
+                                                    }}
+                                                >
+                                                    <Typography textAlign="center">
+                                                        {setting.text}
+                                                    </Typography>
+                                                </Link>
+                                            </MenuItem>
+                                        ))}
+                                    </Menu>
+                                </Box>
+                            </Toolbar>
+                        </AppBar>
+                        <div className="container">
+                            <DrawerLeft />
+                            {id === "dashboard" && <Dashboard />}
+                            {id === "account" && <Account />}
+                        </div>
+                    </>
+                )}
+                <Footer />
+            </>
+        );
+    };
+
+    return (
+        <div className="App">
+            <Routes>
+                <Route element={_renderRouteElement("login")} path="/login" />
+                <Route element={<Logout />} path="/logout" />
+                <Route element={_renderRouteElement("dashboard")} path="/" />
+                <Route
+                    element={_renderRouteElement("account")}
+                    path="/account"
+                />
+                <Route element={_renderRouteElement("notFound")} path="*" />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/configs/apiHelper.ts
+++ src/configs/apiHelper.ts
@@ -2,246 +2,256 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import jwtDecode from "jwt-decode";
 import { store } from "../store/index";
 import {
-  logout,
-  setIsLogined,
-  setOwner,
+    logout,
+    setIsLogined,
+    setOwner,
 } from "../layouts/UserManager/userManagerSlice";
 import { dummyUserOwner } from "../types/userManagerType";
 
 const { dispatch } = store;
 
 export type CustomError = {
-  code?: number;
-  message: string;
+    code?: number;
+    message: string;
 };
 
 export type HttpHeaders = {
-  [key: string]: string;
+    [key: string]: string;
 };
 
 export type RequestConfig = {
-  headers: HttpHeaders;
+    headers: HttpHeaders;
 };
 
 export class ApiConfiguration {
-  accessToken?: string;
+    accessToken?: string;
 }
 
 type Token = {
-  username: string;
-  exp: number;
-  iss: string;
-  aud: string;
+    username: string;
+    exp: number;
+    iss: string;
+    aud: string;
 };
 
 export const getCustomError = (err: any) => {
-  let error: CustomError = {
-    message: "An unknown error occured",
-  };
-  if (
-    err.response &&
-    err.response.data &&
-    err.response.data.error &&
-    err.response.data.message
-  ) {
-    error.code = err.response.data.error;
-    error.message = err.response.data.message;
-  } else if (!err.response && err.message) {
-    error.message = err.message;
-  }
-  return error;
+    let error: CustomError = {
+        message: "An unknown error occured",
+    };
+    if (
+        err.response &&
+        err.response.data &&
+        err.response.data.error &&
+        err.response.data.message
+    ) {
+        error.code = err.response.data.error;
+        error.message = err.response.data.message;
+    } else if (!err.response && err.message) {
+        error.message = err.message;
+    }
+    return error;
 };
 
 export const getFromLocalStorage = async (key: string) => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    if (serializedState === null) {
-      return undefined;
+    try {
+        const serializedState = localStorage.getItem(key);
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (err) {
+        return undefined;
     }
-    return JSON.parse(serializedState);
-  } catch (err) {
-    return undefined;
-  }
 };
 export const saveToLocalStorage = async (key: string, value: any) => {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (err) {
-    // Ignoring write error as of now
-  }
+    try {
+        const serializedState = JSON.stringify(value);
+        localStorage.setItem(key, serializedState);
+    } catch (err) {
+        // Ignoring write error as of now
+    }
 };
 export const clearFromLocalStorage = async (key: string) => {
-  try {
-    localStorage.removeItem(key);
-    return true;
-  } catch (err) {
-    return false;
-  }
+    try {
+        localStorage.removeItem(key);
+        return true;
+    } catch (err) {
+        return false;
+    }
 };
 
 const createAxiosClient = (
-  apiConfiguration: ApiConfiguration
+    apiConfiguration: ApiConfiguration
 ): AxiosInstance => {
-  const newInstance = axios.create({
-    responseType: "json" as const,
-    headers: {
-      "Content-Type": "application/json",
-      ...(apiConfiguration.accessToken && {
-        Authorization: `Bearer ${apiConfiguration.accessToken}`,
-      }),
-    },
-    timeout: 10 * 1000,
-  });
+    const newInstance = axios.create({
+        responseType: "json" as const,
+        headers: {
+            "Content-Type": "application/json",
+            ...(apiConfiguration.accessToken && {
+                Authorization: `Bearer ${apiConfiguration.accessToken}`,
+            }),
+        },
+        timeout: 10 * 1000,
+    });
 
-  newInstance.interceptors.request.use(
-    async (config: AxiosRequestConfig) => {
-      const user = await getFromLocalStorage("user");
-      if (!user) {
-        dispatch(logout());
-        dispatch(setIsLogined(false));
-        return config;
-      }
-      const date = new Date();
-      const decodeToken = jwtDecode<Token>(user?.AccessToken);
-
-      if (decodeToken.exp < date.getTime() / 1000) {
-        try {
-          var res = await axios.post(
-            `${process.env.REACT_APP_DEVELOPMENT_API_ENDPOINT}/api/UserManagers/refreshtoken/employee`,
-            "",
-            {
-              headers: { Authorization: `Bearer ${user?.RefreshToken}` },
+    newInstance.interceptors.request.use(
+        async (config: AxiosRequestConfig) => {
+            const user = await getFromLocalStorage("user");
+            if (!user) {
+                dispatch(logout());
+                dispatch(setIsLogined(false));
+                return config;
             }
-          );
+            const date = new Date();
+            const decodeToken = jwtDecode<Token>(user?.AccessToken);
 
-          if (res.data) {
-            saveToLocalStorage("user", { ...res.data, type: user?.type });
-            dispatch(setOwner({ ...res.data, type: `${user?.type}` }));
-          }
+            if (decodeToken.exp < date.getTime() / 1000) {
+                try {
+                    var res = await axios.post(
+                        `${process.env.REACT_APP_DEVELOPMENT_API_ENDPOINT}/api/UserManagers/refreshtoken/employee`,
+                        "",
+                        {
+                            headers: {
+                                Authorization: `Bearer ${user?.RefreshToken}`,
+                            },
+                        }
+                    );
 
-          if (config.headers) {
-            config.headers["Authorization"] = `Bearer ${
-              res ? res.data.AccessToken : user.AccessToken
-            }`;
-          }
+                    if (res.data) {
+                        saveToLocalStorage("user", {
+                            ...res.data,
+                            type: user?.type,
+                        });
+                        dispatch(
+                            setOwner({ ...res.data, type: `${user?.type}` })
+                        );
+                    }
 
-          dispatch(setIsLogined(true));
-          return config;
-        } catch (err) {
-          clearFromLocalStorage("user");
-          dispatch(setOwner(dummyUserOwner));
-          dispatch(logout());
-          dispatch(setIsLogined(false));
+                    if (config.headers) {
+                        config.headers["Authorization"] = `Bearer ${
+                            res ? res.data.AccessToken : user.AccessToken
+                        }`;
+                    }
+
+                    dispatch(setIsLogined(true));
+                    return config;
+                } catch (err) {
+                    clearFromLocalStorage("user");
+                    dispatch(setOwner(dummyUserOwner));
+                    dispatch(logout());
+                    dispatch(setIsLogined(false));
+                }
+            }
+            return config;
+        },
+        (_err) => {
+            clearFromLocalStorage("user");
+            dispatch(setOwner(dummyUserOwner));
+            dispatch(logout());
+            dispatch(setIsLogined(false));
         }
-      }
-      return config;
-    },
-    (_err) => {
-      clearFromLocalStorage("user");
-      dispatch(setOwner(dummyUserOwner));
-      dispatch(logout());
-      dispatch(setIsLogined(false));
-    }
-  );
+    );
 
-  return newInstance;
+    return newInstance;
 };
 
 export interface IApiClient {
-  post<TRequest, TResponse>(
-    path: string,
-    object: TRequest,
-    config?: RequestConfig
-  ): Promise<TResponse>;
-  patch<TRequest, TResponse>(
-    path: string,
-    object: TRequest
-  ): Promise<TResponse>;
-  put<TRequest, TResponse>(path: string, object: TRequest): Promise<TResponse>;
-  get<TResponse>(path: string): Promise<TResponse>;
+    post<TRequest, TResponse>(
+        path: string,
+        object: TRequest,
+        config?: RequestConfig
+    ): Promise<TResponse>;
+    patch<TRequest, TResponse>(
+        path: string,
+        object: TRequest
+    ): Promise<TResponse>;
+    put<TRequest, TResponse>(
+        path: string,
+        object: TRequest
+    ): Promise<TResponse>;
+    get<TResponse>(path: string): Promise<TResponse>;
 }
 
 export const get = async <TResponse>(path: string): Promise<TResponse> => {
-  try {
-    const userStorage = await getFromLocalStorage("user");
-    const newAxios = createAxiosClient({
-      accessToken: userStorage?.AccessToken,
-    });
-    const response = await newAxios.get<TResponse>(path);
-    return response.data;
-  } catch (error) {
-    // throw new Error(JSON.stringify(error));
-  }
-  return {} as TResponse;
+    try {
+        const userStorage = await getFromLocalStorage("user");
+        const newAxios = createAxiosClient({
+            accessToken: userStorage?.AccessToken,
+        });
+        const response = await newAxios.get<TResponse>(path);
+        return response.data;
+    } catch (error) {
+        // throw new Error(JSON.stringify(error));
+    }
+    return {} as TResponse;
 };
 
 export const put = async <TRequest, TResponse>(
-  path: string,
-  payload: TRequest
+    path: string,
+    payload: TRequest
 ): Promise<TResponse> => {
-  try {
-    const userStorage = await getFromLocalStorage("user");
-    const newAxios = createAxiosClient({
-      accessToken: userStorage?.AccessToken,
-    });
-    const response = await newAxios.put<TRequest, TResponse>(path, payload);
-    return response;
-  } catch (error) {
-    // throw new Error(JSON.stringify(error));
-  }
-  return {} as TResponse;
+    try {
+        const userStorage = await getFromLocalStorage("user");
+        const newAxios = createAxiosClient({
+            accessToken: userStorage?.AccessToken,
+        });
+        const response = await newAxios.put<TRequest, TResponse>(path, payload);
+        return response;
+    } catch (error) {
+        // throw new Error(JSON.stringify(error));
+    }
+    return {} as TResponse;
 };
 
 export const deleteResource = async (path: string): Promise<any> => {
-  try {
-    const userStorage = await getFromLocalStorage("user");
-    const newAxios = createAxiosClient({
-      accessToken: userStorage?.AccessToken,
-    });
-    const response = await newAxios.delete(path);
-    return response;
-  } catch (error) {
-    // throw new Error(JSON.stringify(error));
-  }
+    try {
+        const userStorage = await getFromLocalStorage("user");
+        const newAxios = createAxiosClient({
+            accessToken: userStorage?.AccessToken,
+        });
+        const response = await newAxios.delete(path);
+        return response;
+    } catch (error) {
+        // throw new Error(JSON.stringify(error));
+    }
 };
 
 export const deleteData = async <TRequest, TResponse>(
-  path: string,
-  payload: TRequest
+    path: string,
+    payload: TRequest
 ): Promise<TResponse> => {
-  try {
-    const userStorage = await getFromLocalStorage("user");
-    const newAxios = createAxiosClient({
-      accessToken: userStorage?.AccessToken,
-    });
-    const response = await newAxios.delete<TRequest, TResponse>(path, {
-      data: payload,
-    });
-    return response;
-  } catch (error) {
-    // throw new Error(JSON.stringify(error));
-  }
-  return {} as TResponse;
+    try {
+        const userStorage = await getFromLocalStorage("user");
+        const newAxios = createAxiosClient({
+            accessToken: userStorage?.AccessToken,
+        });
+        const response = await newAxios.delete<TRequest, TResponse>(path, {
+            data: payload,
+        });
+        return response;
+    } catch (error) {
+        // throw new Error(JSON.stringify(error));
+    }
+    return {} as TResponse;
 };
 
 export const post = async <TRequest, TResponse>(
-  path: string,
-  payload?: TRequest,
-  config?: RequestConfig
+    path: string,
+    payload?: TRequest,
+    config?: RequestConfig
 ): Promise<TResponse> => {
-  try {
-    const userStorage = await getFromLocalStorage("user");
-    const newAxios = createAxiosClient({
-      accessToken: userStorage?.AccessToken,
-    });
-    const response = config
-      ? await newAxios.post<TRequest, TResponse>(path, payload, config)
-      : await newAxios.post<TRequest, TResponse>(path, payload);
+    try {
+        const userStorage = await getFromLocalStorage("user");
+        const newAxios = createAxiosClient({
+            accessToken: userStorage?.AccessToken,
+        });
+        const response = config
+            ? await newAxios.post<TRequest, TResponse>(path, payload, config)
+            : await newAxios.post<TRequest, TResponse>(path, payload);
 
-    return response;
-  } catch (error) {
-    // throw new Error(JSON.stringify(error));
-  }
-  return {} as TResponse;
+        return response;
+    } catch (error) {
+        // throw new Error(JSON.stringify(error));
+    }
+    return {} as TResponse;
 };
